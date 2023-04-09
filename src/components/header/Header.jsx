2cfe035062dc4e79839c97010d079613190cc806
ubkbek <ulugbekjovliyev01001@gmail.com>
2023-04-09 11:42:20 +0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logoImage from "./../../assets/images/logo.png";
+import burgerMenu from "./../../assets/images/burger-menu.png";
+import cross from "./../../assets/images/x.png";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container header__container">
+        <Link className="header__logo-link" to={"/"}>
+          <img
+            src={logoImage}
+            alt="Logotip of Renessans learning centre"
+            width={180}
+          />
+        </Link>
+
+        <div className="header__nav-wrapper">
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item">
+                <NavLink className="header__nav-link">Home</NavLink>
+              </li>
+              <li className="header__nav-item">
+                <NavLink className="header__nav-link">About us</NavLink>
+              </li>
+              <li className="header__nav-item">
+                <NavLink className="header__nav-link">Courses</NavLink>
+              </li>
+              <li className="header__nav-item">
+                <NavLink className="header__nav-link">Contacts</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <button className="sign-in-btn">Kirish</button>
+        </div>
+
+        <button className="header__btn">
+          <img
+            className="burger__btn"
+            width={20}
+            src={burgerMenu}
+            alt="burger menu"
+          />
+          <img className="close__btn" src={cross} alt="close menu" width={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
